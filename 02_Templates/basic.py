257278b24342c02f0502d734f143faff25a62cfa
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-12-13 16:42:04 -0300

    Adicionado Template Control Flow

--- 02_Templates/basic.py
+++ 02_Templates/basic.py
@@ -4,10 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    user_logged_in = True
     nome = 'breno'
     letras = list(nome.capitalize())
     pup_dict = {'pup_name': 'Sammy'}
-    return render_template('basic.html', nome=nome, letras=letras, pup_dict=pup_dict)
+    puppies = ['Fluffy', 'Rufus', 'Spike']
+    return render_template('basic.html', nome=nome, letras=letras, pup_dict=pup_dict, puppies=puppies, user_logged_in=user_logged_in)
 
 if __name__ == "__main__":
     app.run(debug=True)
