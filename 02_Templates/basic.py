7fe1e71daf4ef0afbad5cfad11b4ca4d1f439f89
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-12-13 16:28:37 -0300

    Adicionando Jinja

--- 02_Templates/basic.py
+++ 02_Templates/basic.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('basic.html')
+    nome = 'breno'
+    letras = list(nome.capitalize())
+    pup_dict = {'pup_name': 'Sammy'}
+    return render_template('basic.html', nome=nome, letras=letras, pup_dict=pup_dict)
 
 if __name__ == "__main__":
     app.run(debug=True)
